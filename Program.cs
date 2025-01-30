be4350d55be9c49f15a4c4f899c65605e509c656
JulesRuby <JulesRuby@users.noreply.github.com>
2025-01-29 21:43:25 -0700

    Okay, looks like we've worked out a half-decent method for int validation. Used it in loop.

--- Program.cs
+++ Program.cs
@@ -2,17 +2,33 @@
 {
     internal class Program
     {
+
+        // Let's see if I can begin abtracting a validator function to make these do...while loops a bit easier to look at and code. 
+        public static bool validateInterger (string userInput, out int value)
+        {
+            if (int.TryParse(userInput, out value) && value > 0)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+
+            //int.TryParse(userInput, out value) && value > 0 ? true : false;
+        }
+        
+
         static void Main(string[] args)
         {
+            // PART 1
             string? lowInput = String.Empty;
             string? highInput = String.Empty;
             int lowInt;
             int highInt;
             bool passIntCheck;
-
-
-            Console.WriteLine("Welcome to this really cool, sexy and popular program!");
-            Console.WriteLine("Enter a low number: ");
+           
+            Console.WriteLine("Welcome to this really cool, sexy and popular program!\n\n");
+            Console.Write("Enter a low number: ");
 
             do
             {
@@ -26,7 +42,7 @@
             } while (!passIntCheck);
 
 
-            Console.WriteLine("Enter a high number: ");
+            Console.Write("Enter a high number: ");
 
             do
             {
@@ -42,6 +58,67 @@
             int absoluteValDifference = Math.Abs(highInt - lowInt);
 
             Console.WriteLine($"The difference between these two values is {absoluteValDifference}");
+
+
+            //PART 2
+            // insert task 2 variables with validation checks here
+            int[] positiveIntegers = { 0, 0 };
+            positiveIntegers[0] = 1;
+            int positiveInteger = 0;
+            bool isPosInt = false;
+            bool isHigherThanLow = false;
+
+            do
+            {
+                // Reset validation boolean on each loop
+                isPosInt = false;
+
+                string? userInput = String.Empty;
+
+                Console.Write("Enter a low positive integer: ");
+                userInput = Console.ReadLine();
+                passIntCheck = validateInterger(userInput, out positiveInteger);
+
+                if (passIntCheck)
+                {
+                    positiveIntegers[0] = positiveInteger;
+                    isPosInt = true;
+                } else
+                {
+                    Console.WriteLine("Your input must be a valid integer and also positive!");
+                }
+
+            } while (!isPosInt);
+
+            do
+            {
+                // Reset both validation booleans on higher number loop
+                isPosInt = isHigherThanLow = false;
+                string? userInput = String.Empty;
+
+                Console.Write("Enter a positive higher value integer: ");
+                userInput = Console.ReadLine();
+                passIntCheck = validateInterger(userInput, out positiveInteger);
+
+                if (!passIntCheck)
+                {
+                    Console.WriteLine("Your input must be a valid integer and also positive!");
+                    // Skip over the remaining loop 
+                    continue;
+                }
+
+                positiveIntegers[1] = positiveInteger;
+                
+                if (positiveIntegers[1] <= positiveIntegers[0])
+                {
+                    Console.WriteLine("Your integer must be of higher value than your input from the previous step!");
+                }
+                else
+                {
+                    isPosInt = isHigherThanLow = true;
+                }
+
+            } while (!isPosInt || !isHigherThanLow);
         }
     }
 }
